bec536f0d93a8f9afa26a4ba4de5697598da164a
HelloEnkio <hello@enkio.fr>
2025-04-23 11:12:09 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig(({ mode }) => {
       proxy: {
         '/api': {
           target: 'http://localhost:3000',
-          changeOrigin: True,
+          changeOrigin: true,
         },
       },
     },
